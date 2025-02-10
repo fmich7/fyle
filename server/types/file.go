3491cd8be433cdec8556b91fa2b8e85d782b1d0a
fmich7 <michalak.filip.04@gmail.com>
2025-02-10 12:05:29 +0100

    Added file upload endpoint to server

---
+++ server/types/file.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"mime/multipart"
+	"net/textproto"
+)
+
+// File represents a file object
+type File struct {
+	Filename string
+	Header   textproto.MIMEHeader
+	Size     int64
+	Data     multipart.File
+	Owner    string
+	Location string
+}
+
+// NewFile creates a new File object
+func NewFile(header *multipart.FileHeader, file multipart.File, owner string, location string) *File {
+	return &File{
+		Filename: header.Filename,
+		Header:   header.Header,
+		Size:     header.Size,
+		Data:     file,
+		Owner:    owner,
+		Location: location,
+	}
+}
