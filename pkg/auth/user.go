07abfdfbccc9454fb54bba86b7b8c659dd41f493
fmich7 <michalak.filip.04@gmail.com>
2025-02-27 22:47:56 +0100

    add user object and password hashing functions

--- pkg/auth/user.go
+++ pkg/auth/user.go
@@ -1,7 +1,40 @@
 package auth
 
+import "golang.org/x/crypto/bcrypt"
+
 // User type
 type User struct {
+	ID       int
 	Username string
 	Password string
+}
+
+// NewUser creates new user with hashed password
+func NewUser(username, password string) (*User, error) {
+	// hash password
+	hashedPassword, err := hashPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		Username: username,
+		Password: hashedPassword,
+	}, nil
+
+}
+
+// CheckPassword checks if user provided valid password
+func CheckPassword(storedPassword, enteredPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(enteredPassword))
+	return err != nil
+}
+
+// hashPassword hashes given password
+func hashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
 }
